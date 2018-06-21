a6ebedfa607660ed27dd1da1962b42057b8589c1
chrisdibby <chrisdibby@gmail.com>
2018-06-21 16:39:07 +0300

    some refactoring

---
+++ src/main/java/logic/cdi/UserCDI.java
@@ -0,0 +1,69 @@
+package logic.cdi;
+
+import logic.domain.UserEntity;
+import logic.ejb.UserBean;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+
+@Named
+@SessionScoped
+public class UserCDI implements Serializable{
+
+    private String login;
+    private String password;
+
+    private boolean loginSuccess;
+    private boolean createSuccess;
+
+    @EJB
+    private UserBean userBean;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoginSuccess() {
+        return loginSuccess;
+    }
+
+    public void setLoginSuccess(boolean loginSuccess) {
+        this.loginSuccess = loginSuccess;
+    }
+
+    public boolean isCreateSuccess() {
+        return createSuccess;
+    }
+
+    public void setCreateSuccess(boolean createSuccess) {
+        this.createSuccess = createSuccess;
+    }
+
+    public void checkPassword(){
+        loginSuccess = userBean.checkPassword(login, password);
+    }
+
+    public void createUser(){
+        createSuccess = userBean.createUser(login, password);
+    }
+
+    public List<UserEntity> getAllUsers(){
+        return userBean.getAllUsers();
+    }
+}
---
+++ src/main/java/logic/ejb/UserBean.java
@@ -0,0 +1,58 @@
+package logic.ejb;
+
+import logic.domain.UserEntity;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+
+@Stateless
+public class UserBean {
+
+    @PersistenceContext(unitName = "MyMasteringPU")
+    private EntityManager entityManager;
+
+    public boolean checkPassword(String login, String password){
+        if(StringUtils.isEmpty(login) || StringUtils.isEmpty(password)){
+            return false;
+        }
+
+        UserEntity userEntity = entityManager.find(UserEntity.class, login);
+        if(userEntity == null){
+            return false;
+        }
+
+        if(password.equals(userEntity.getPassword())){
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String login, String password){
+        if(StringUtils.isEmpty(login) || StringUtils.isEmpty(password)){
+            return false;
+        }
+
+        UserEntity userEntity = entityManager.find(UserEntity.class, login);
+        if(userEntity != null){
+            return false;
+        }
+
+        userEntity = new UserEntity();
+        userEntity.setLogin(login);
+        userEntity.setPassword(password);
+        entityManager.persist(userEntity);
+
+        return true;
+    }
+
+    public List<UserEntity> getAllUsers(){
+        Query query = entityManager.createQuery("select entity from UserEntity entity");
+        return query.getResultList();
+    }
+}
