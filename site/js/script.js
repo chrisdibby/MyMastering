089dd6aacc8606847aca880d7264a976ad763978
Татьяна Нарубина <tanyxxxl91@mail.ru>
2018-07-07 20:24:39 +0300

    Удалила лишнюю папку site

--- site/js/script.js
+++ site/js/script.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top) {
-          $('#navbar').css('background-color', 'rgba(255, 255, 255, .5)'), $('#navbar').css('box-shadow', '0 3px 23px 7px rgba(255, 255, 255, .5)');
+          $('#navbar').css('background-color', 'rgba(255, 255, 255, .5)'), $('#navbar').css('box-shadow', '0 3px 23px 7px rgba(255, 255, 255, .5)'), $('#navbar').css('transition', 'all 0.5s ease-in-out');
+       } else {
+          $('#navbar').css('background-color', 'transparent'), $('#navbar').css('box-shadow', 'none'), $('#navbar').css('transition', 'all 0.3s ease-in-out');
        }
    });
 });
